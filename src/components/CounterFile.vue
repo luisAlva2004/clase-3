5cf62c74cdf4f8423d9cfd853f7531b2e9a8fbde
luisAlva2004 <luisgera.alvarado@gmail.com>
2024-02-16 13:14:01 -0600

    first commit

---
+++ src/components/CounterFile.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1>{{ counter }}</h1>
+    <button @click="incremento()">+</button>
+    <button @click="reset()">Reset</button>
+    <button @click="decremento()">-</button>
+</template>
+
+<script>
+import {ref} from 'vue';
+
+export default{
+    setup(){
+        const counter = ref(0);
+        return {counter};
+    },
+
+    methods: {
+        incremento(){
+            this.counter++;
+        },
+
+        reset(){
+            this.counter = 0;
+        },
+
+        decremento(){
+            this.counter--;
+        }
+    }
+}
+</script>
